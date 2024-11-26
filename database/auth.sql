a7613d884c6960c8b254c387eaca4252c7fded82
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2024-11-26 15:22:25 +0530

    update user schema

--- database/auth.sql
+++ database/auth.sql
@@ -1,4 +1,4 @@
-CREATE TABLE USER (
+CREATE TABLE USERS (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     profilePic VARCHAR(255),  -- Stores fileuploadId, you can adjust size as needed
     email varchar(255) NOT NULL UNIQUE,
@@ -7,7 +7,7 @@ CREATE TABLE USER (
     passwordHash VARCHAR(255) NOT NULL,
     loginURL VARCHAR(15) NOT NULL,
     accessLevel TINYINT(2) NOT NULL,  -- Range 1-12
-    lastLogin TIMESTAMP NULL DEFAULT NULL,
+    lastLogin TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     status ENUM('active', 'banned', 'restricted') NOT NULL DEFAULT 'active',
     reputationPoint ENUM('newbie', 'expert', 'intermediate') NOT NULL DEFAULT 'newbie',
     strikeCount INT DEFAULT 0,
@@ -15,9 +15,10 @@ CREATE TABLE USER (
     credits DECIMAL(10,2) DEFAULT 0.00,
     timezone VARCHAR(50) DEFAULT 'UTC',
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted TINYINT (2) DEFAULT 0
 );
 
-CREATE TABLE PASSWORD (
+CREATE TABLE PASSWORDS (
     id INT AUTO_INCREMENT PRIMARY KEY,
     userId INT NOT NULL,
     passwordHash VARCHAR(255) NOT NULL,
@@ -29,8 +30,10 @@ CREATE TABLE PASSWORD (
 CREATE TABLE InviteCodes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     code VARCHAR(50) NOT NULL UNIQUE,
-    userId INT NOT NULL,
+    generatorId INT NOT NULL,
     used TINYINT(1) DEFAULT 0,  -- 0 for not used, 1 for used
+    usedBy INT NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (userId) REFERENCES USER(ID) ON DELETE CASCADE
+    FOREIGN KEY (generatorId) REFERENCES USER(ID) ON DELETE CASCADE,
+    FOREIGN KEY (usedBy) REFERENCES USER(ID) ON DELETE CASCADE,
 );
