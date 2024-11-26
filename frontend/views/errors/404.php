3230ea25c8a93049761966bd374ddfacc2f93808
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2024-11-26 15:29:47 +0530

    chore(frontend): add new error views directory

---
+++ frontend/views/errors/404.php
@@ -0,0 +1,5 @@
+<?php require(base_path("/frontend/views/partials/header.php")); ?>
+<?php require(base_path("/frontend/views/partials/navbar.php")); ?>
+
+<?= "404 Page Not Found"?>
+<?php require(base_path("/frontend/views/partials/footer.php")); ?>
