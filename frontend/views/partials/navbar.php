ae17ff0f12843a78ceeca67cb3ae54cc2b52a69c
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2024-11-28 04:14:37 +0530

    some change in header and navbar

--- frontend/views/partials/navbar.php
+++ frontend/views/partials/navbar.php
@@ -14,12 +14,4 @@
    <li>
       <button type="button" id="logout">Sign out</button>
    </li>
-   <?php if (parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) === '/thread'): ?>
-      <li>
-         <form action="" method="post">
-            <input type="hidden" name="_method" value="DELETE">
-            <button type="submit">Delete</button>
-         </form>
-      </li>
-   <?php endif ?>
 </ul>
