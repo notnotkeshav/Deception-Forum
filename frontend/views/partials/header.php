91a18c8b45eac45f48fc5ec8af7cc56abd8edd2e
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-09 20:42:28 +0530

    add bootstrap to views

--- frontend/views/partials/header.php
+++ frontend/views/partials/header.php
@@ -4,6 +4,7 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="/public/stylesheets/bootstrap.css">
    <link rel="stylesheet" href="/public/stylesheets/styles.css">
    <link rel="shortcut icon" href="/public/images/favicon.ico" type="image/x-icon">
    <script src="/public/javascripts/jquery-3.7.1.min.js"></script>
