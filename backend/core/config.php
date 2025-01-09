7a555ed7faf41fa94d26fb2085bdb179d1cb265c
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-10 00:15:34 +0530

    add mailer to project

--- backend/core/config.php
+++ backend/core/config.php
@@ -10,5 +10,14 @@ return [
       "pre_render"=>[
          "token"=>"",
       ]
-   ]
+   ],
+   "mailer" => [
+       "host" => getenv("MAILER_HOST"),
+       "port" => getenv("MAILER_PORT"),
+       "username" => getenv("MAILER_USERNAME"),
+       "password" => getenv("MAILER_PASSWORD"),
+       "encryption" => getenv("MAILER_ENCRYPTION"), // Optional: "ssl" or "tls"
+       "fromEmail" => getenv("MAILER_FROM_EMAIL"),
+       "fromName" => getenv("MAILER_FROM_NAME"),
+   ],
 ];
